2c692e76da2a644ec13e4f6a02aeb5609460c411
Vincent Desmouceaux <desmontvincent@gmail.com>
2023-11-01 17:39:33 +0100

    mise en place des composants

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="menu-item">
+      <img class="menu-item__image" :src="image.source" :alt="image.alt" />
+      <div>
+        <h3>{{ name }}</h3>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <div>
+          <label for="add-item-quantity">Quantity: {{ quantity }}</label>
+          <input v-model.number="localQuantity" id="add-item-quantity" type="number" />
+          <button @click="addToShoppingCart(localQuantity)">
+            Add to Shopping Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MenuItem',
+    props: ['addToShoppingCart', 'image', 'inStock', 'name', 'quantity'],
+    data() {
+      return {
+        localQuantity: this.quantity
+      };
+    },
+    watch: {
+      quantity(newVal) {
+        this.localQuantity = newVal;
+      }
+    }
+  }
+  </script>
+  
+  <style></style>
+  
